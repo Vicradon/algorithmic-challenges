c33ef144c15608d9e79464155ae0bfec7b7cf757
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-02-25 12:11:17 +0100

    Create simpleRecursionExamples.js:

---
+++ simpleRecursionExamples.js
@@ -0,0 +1,14 @@
+
+function reverseString(str){
+  if (str.length < 2) return str;
+  return reverseString(str.slice(1, str.length)) + str[0];
+}
+
+console.log(reverseString("stuff"));
+
+function printStuff(arr){
+  if (arr.length < 2) return arr;
+  return `${arr[0]}\n${printStuff(arr.slice(1, arr.length))}`;
+}
+
+console.log(printStuff([1, 2, 3]));
